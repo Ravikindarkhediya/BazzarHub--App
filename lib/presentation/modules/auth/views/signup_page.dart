ee9b2a7a6cc800a5e8bcb70f6a82f95cd121fdcd
Ravi <uttamdhameshiyacreativeinfoway@gmail.com>
2025-11-08 16:05:43 +0530

    error solve

--- lib/presentation/modules/auth/views/signup_page.dart
+++ lib/presentation/modules/auth/views/signup_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:bazzar_hub_app/presentation/modules/auth/views/social_tab.dart';
 import 'package:dio/dio.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_sign_in/google_sign_in.dart';
