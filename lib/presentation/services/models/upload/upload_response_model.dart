42936ab2ac0af60f6ab9a43fd9ef966f16eb9efb
Ravi <uttamdhameshiyacreativeinfoway@gmail.com>
2025-11-20 10:38:20 +0530

    UploadResponseModel class commited

---
+++ lib/presentation/services/models/upload/upload_response_model.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'upload_response_model.g.dart';
+
+@JsonSerializable()
+class UploadResponseModel {
+  @JsonKey(name: 'url', defaultValue: '')
+  final String url;
+
+  const UploadResponseModel({
+    this.url = '',
+  });
+
+  factory UploadResponseModel.fromJson(Map<String, dynamic> json) =>
+      _$UploadResponseModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UploadResponseModelToJson(this);
+}
+
---
+++ lib/presentation/services/models/upload/upload_response_model.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'upload_response_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UploadResponseModel _$UploadResponseModelFromJson(Map<String, dynamic> json) =>
+    UploadResponseModel(url: json['url'] as String? ?? '');
+
+Map<String, dynamic> _$UploadResponseModelToJson(
+  UploadResponseModel instance,
+) => <String, dynamic>{'url': instance.url};
